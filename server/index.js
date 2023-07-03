eedb9400b683590f1333f4a992af89894f0b3aba
PaulaPerazzo <mpperazzosampaio@gmail.com>
2023-07-03 12:08:48 -0300

    (main): create schedule succesfully implemented

--- server/index.js
+++ server/index.js
@@ -61,7 +61,29 @@ app.post("/login", (req, res) => {
             _email: result[0].email,
         },
     });
-})
+});
+
+app.post("/schedule/create", (req, res) => {
+    const { userId, timezone, schedule } = req.body;
+    let result = database.filter((db) => db.id === userId);
+    result[0].timezone = timezone;
+    result[0].schedule = schedule;
+    res.json({ message: "OK" });
+});
+
+app.get("/schedules/:id", (req, res) => {
+    const { id } = req.params;
+    let result = database.filter((db) => db.id === id);
+    if (result.length === 1) {
+        return res.json({
+            message: "Schedules successfully retrieved!",
+            schedules: result[0].schedule,
+            username: result[0].username,
+            timezone: result[0].timezone,
+        });
+    }
+    return res.json({ error_message: "Sign in again, an error occured..." });
+});
 
 app.listen(port, () => {
     console.log('server running');
